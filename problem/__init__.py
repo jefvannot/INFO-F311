4c7f37b07d85e20f0db58f87b792b39a95745832
jefvannot <gwenda.frocrain@gmail.com>
2024-09-25 14:48:00 +0200

    Delete problem/__init__.py

--- problem/__init__.py
+++
@@ -1,6 +0,0 @@
-from .problem import SearchProblem
-from .exit_problem import ExitProblem
-from .corner_problem import CornerProblem, CornerState
-from .gem_problem import GemProblem
-
-__all__ = ["SearchProblem", "ExitProblem", "CornerProblem", "CornerState", "GemProblem"]
